90c3bdd6c263eb5d5310ce1dcbcf04fbecadcf90
Brar21 <103635395+Brar21@users.noreply.github.com>
2023-01-31 21:30:33 +0530

    trying to deploy static website

--- src/Pages/Test.jsx
+++ src/Pages/Test.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React from 'react';
 import { Link } from '@chakra-ui/react';
 import { Text, Button, Box, useDisclosure } from '@chakra-ui/react';
 import { SlSpeedometer } from "react-icons/sl";
@@ -14,59 +14,13 @@ import {
     AlertDialogOverlay,
 } from '@chakra-ui/react';
 import { Textarea } from '@chakra-ui/react';
-import Texts from "../Components/Text";
-import Speed from '../Components/Speed';
-import getText from '../Components/getText';
-const intialState = {
-    text:getText(),
-    useInput: '',
-    symbols: 0,
-    sec: 0,
-    started: false,
-    finish: false,
-}
+
 export const TestSpeed = () => {
    
-   const [state,setState]=useState(intialState)
     
     const { isOpen, onOpen, onClose } = useDisclosure();
-    const onRestart = () => {
-        setState(intialState)
-        onClose();
-    }
-    const onUserInputChange = (e) => {
-        const val = e.target.value;
-        setTime();
-        onFinish(val);
-        setState({
-            useInput: val,
-            symbols:countSymbols(val)
-        })
-    }
-   const  countSymbols=(useInput)=>{
-        const text = state.text.replace(" ", '');
-       return useInput.replace(' ', '').split('').filter((paragraph, initial) => paragraph === text[initial]).length;
-    }
-    var interval;
-    const setTime=()=>{
-        if (!state.started) {
-            setState({ started: true });
-            interval = setInterval(() => {
-                setState(prevProps => {
-                    return { sec: prevProps.sec + 1 }
-                })
-            }, 1000)
-        }
-    }
- 
-    const onFinish = (useInput) => {
-        if (useInput === state.text) {
-            clearInterval(interval)
-            setState({
-                finish:true
-            })
-       }
-   }
+   
+  
    
     return (
         <Box w='100%' bg='#577b87'>
@@ -74,10 +28,9 @@ export const TestSpeed = () => {
                 <Box bg="white" h='45vh' borderRadius={10} w='80%' m='auto'>
                     {/* <Text textAlign='start' p='1.5rem' fontSize={25} color='gray.500'> */}
                     {/* <Text fontSize={25} textAlign='start' p='1.5rem'> */}
-                        <Texts text={state.text} useInput={state.useInput} readOnly={state.finish} />                    
                     {/* </Text> */}
                 {/* </Text> */}
-                    <Textarea placeholder={'Start Typing....'} value={state.useInput} onChange={onUserInputChange} border='none' mt={'5px'}>
+                    <Textarea placeholder={'Start Typing....'}  border='none' mt={'5px'}>
                 </Textarea>
 
                 </Box>
@@ -87,7 +40,6 @@ export const TestSpeed = () => {
                         {/* <Text fontFamily='mono' fontWeight='800' fontSize={28} m='8px' color='facebook.200' > */}
                     {/* </Text> */}
                 {/* </span> */}
-                    <Speed sec={state.sec} symbols={state.symbols} />
 
                     <Text fontSize={25} fontWeight='700' color='whatsapp.500' display="flex" gap={3}><Text pt='5px' fontSize={28} color='white'><GiArcheryTarget /></Text>ACCURACY</Text>
                     <span><Text fontFamily='mono' fontWeight='800' fontSize={28} m='8px'  color='facebook.200'>100%</Text></span>
@@ -109,7 +61,7 @@ export const TestSpeed = () => {
                                 </AlertDialogBody>
 
                                 <AlertDialogFooter m='1rem auto'>
-                                    <Button onClick={onRestart} colorScheme='whatsapp' p='2rem 2.5rem' fontSize={30}>
+                                    <Button  colorScheme='whatsapp' p='2rem 2.5rem' fontSize={30}>
                                         <Link herf="/testspeed/test">Start Typing Again</Link>
                                     </Button>
 
