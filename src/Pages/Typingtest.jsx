28a787e04973d94fa47b1fb2d87e808a63ef28ae
Brar21 <103635395+Brar21@users.noreply.github.com>
2023-02-03 14:02:53 +0530

    make repsonsive this site

--- src/Pages/Typingtest.jsx
+++ src/Pages/Typingtest.jsx
@@ -49,15 +49,15 @@ function Test() {
                         >                                <AlertDialogOverlay  >
                                 <AlertDialogContent  >
                                     <AlertDialogHeader fontSize='lg' fontWeight='bold'>
-                                        <Text bg='beige' w='150px' h='150px' borderRadius='50%' m='auto' p="5% 5.8%"><VscDebugStart fontSize={90} fill='green' /></Text>
+                                        <Text bg='beige' w='150px' h='150px' borderRadius='50%' m='auto' p={{base:'10% 10.8%',lg:"5% 5.8%"}}><VscDebugStart fontSize={90} fill='green' /></Text>
                                     </AlertDialogHeader>
 
-                                    <AlertDialogBody m='auto' fontSize={23} fontWeight='600'>
+                                    <AlertDialogBody m='auto' fontSize={{base:15,lg:23}} fontWeight='600'>
                                         <Text display='flex'> You can <span > <Link to='/donate'><Text pl={2} _hover={{ color: "blue", textDecoration: 'underline' }}>thanks us and Donate</Text></Link></span></Text>
                                     </AlertDialogBody>
 
                                     <AlertDialogFooter m='1rem auto'>
-                                        <Button onClick={onClose} colorScheme='whatsapp' p='2rem 2.5rem' fontSize={30}>
+                                        <Button onClick={onClose} colorScheme='whatsapp' p='2rem 2.5rem' fontSize={{base:18,lg:30}}>
                                             <Link to="/testspeed/test">Start Typing Test</Link>
                                         </Button>
 
@@ -106,7 +106,7 @@ function Test() {
                             </Table>
                         </TableContainer>
                     </Box>
-                    <Box width={{base:'40%',lg:'30%'}} m={{base:' 1rem auto',lg:'1rem'}}>
+                    <Box width={{base:'40%',sm:"50%",lg:'30%'}} m={{base:' 1rem auto',sm:'auto',lg:'1rem'}}>
                         <Text textAlign='start' fontSize={18} color='black'>
                             You can take the test as many times as you want! Only the best score will count towards your certification, there’s no need to worry about making a mistake.</Text>
                         <Box display='flex' h='5rem' mt={5} color='blue' fontSize={20} fontWeight='500' textAlign='center' _hover={{ textDecoration: "underline", gap: "1.5rem" }} gap='0.5rem' >
