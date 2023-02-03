d92e842b250a9a3e6130da1306363dbd830825ca
Brar21 <103635395+Brar21@users.noreply.github.com>
2023-02-03 13:50:13 +0530

    make repsonsive this site

--- src/Pages/Typingtest.jsx
+++ src/Pages/Typingtest.jsx
@@ -28,10 +28,10 @@ function Test() {
 
     return (
         <Box w='100%' h='auto'>
-            <Box bg='#577b87' h='90vh'>
-                <Box w='70%' m='auto' display='flex' flexDirection='row' gap={10} p='1rem' >
+            <Box bg='#577b87' h={{base:'120vh',lg:'90vh'}}>
+                <Box w='70%' m='auto' display={{base:'grid',lg:'flex'}} flexDirection='row' gap={10} p='1rem' >
                     <Box>
-                        <Image w='100rem' borderRadius={20} src='https://i.pinimg.com/originals/3f/82/40/3f8240fa1d16d0de6d4e7510b43b37ba.gif' />
+                        <Image w='auto' borderRadius={20} src='https://i.pinimg.com/originals/3f/82/40/3f8240fa1d16d0de6d4e7510b43b37ba.gif' />
                     </Box>
                     <Box m='auto' _disabled='flex' flexDirection='column' >
                         <Heading textAlign='start' color='white'>Speed Test</Heading>
@@ -69,13 +69,13 @@ function Test() {
                 </Box>
             </Box>
             <Box w='70%' bg='white' m='-14rem auto 0' border='2px solid red' borderRadius={15} >
-                <Image w='50%' m='auto' h={400} src='https://img.freepik.com/premium-vector/gold-silver-bronze-medals-flat-style-icon-set_201904-154.jpg?w=2000' />
-                <Box display='flex' w='90%' m='auto' flexDirection='row' gap={10}>
-                    <Box w='70%' m='1rem'>
+                <Image w='50%' m='auto' h={{base:'auto',lg:400}} src='https://img.freepik.com/premium-vector/gold-silver-bronze-medals-flat-style-icon-set_201904-154.jpg?w=2000' />
+                <Box display={{base:'grid',lg:'flex'}} w='90%' m='auto' flexDirection='row' gap={10}>
+                    <Box w={{base:'30%',lg:'70%'}} m='1rem'>
                         <TableContainer>
                             <Table variant='simple'>
 
-                                <Thead w='50%'>
+                                <Thead w={{base:'auto',lg:'50%'}}>
                                     <Tr display='flex' flexDirection='row' w='100%' justifyContent='space-between'>
                                         <Th display='flex' gap='5px' pl="1rem"><GrCertificate fontSize={20} />CERTIFICATE</Th>
                                         <Th display='flex' gap='5px' ml='2rem'><SlSpeedometer fontSize={20} />SPEED</Th>
@@ -83,7 +83,7 @@ function Test() {
                                     </Tr>
                                 </Thead>
                                 <hr />
-                                <Tbody fontWeight='500'>
+                                <Tbody fontWeight='500' color={'black'}>
                                     <Tr display='flex' flexDirection='row' w='100%' justifyContent='space-between' alignItems='center'>
                                         <Td display='flex' textAlign='center'><Image w={30} src='https://cdn.vectorstock.com/i/preview-1x/65/82/silver-rosette-vector-996582.jpg' /><Text pt={2}>PLATINUM</Text></Td>
                                         <Td pl='5px'>70 WPM</Td>
@@ -106,8 +106,8 @@ function Test() {
                             </Table>
                         </TableContainer>
                     </Box>
-                    <Box width='30%' m='1rem'>
-                        <Text textAlign='start' fontSize={18}>
+                    <Box width={{base:'40%',lg:'30%'}} m={{base:' 1rem auto',lg:'1rem'}}>
+                        <Text textAlign='start' fontSize={18} color='black'>
                             You can take the test as many times as you want! Only the best score will count towards your certification, there’s no need to worry about making a mistake.</Text>
                         <Box display='flex' h='5rem' mt={5} color='blue' fontSize={20} fontWeight='500' textAlign='center' _hover={{ textDecoration: "underline", gap: "1.5rem" }} gap='0.5rem' >
                             <Text textAlign='start' pb='5rem' ><Link to='/testspeed/test'>Take Typing Test</Link> </Text><Text pt={1} fontSize={22}><AiOutlineArrowRight /></Text>
