01c02aa0801f5f36de1833b1a668f4e0144dae8b
Brar21 <103635395+Brar21@users.noreply.github.com>
2023-02-05 17:49:03 +0530

    responsive design

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,9 +3,9 @@ import {Box,Text} from "@chakra-ui/react";
 function Footer() {
      
     return (
-        <Box w={"full"}  h={10}  margin={'auto'}>
+        <Box w={"full"}  h={10}  margin={'1rem auto'}>
     <hr color="green" />
-        <Box display={{base:'flex'}} width='70%' gap={7} textAlign='center' flexWrap='wrap' m={'auto'}>
+        <Box display={'flex'} width='60%' gap={7} textAlign='center' flexWrap='wrap' m={'auto'}>
             <h3>For Devlopers</h3>
             <Text>About</Text>
             <Text>High Scores</Text>
