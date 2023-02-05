b3c9d6e5019c19bd610f6a3010ade40ba514bff3
Brar21 <103635395+Brar21@users.noreply.github.com>
2023-02-05 17:46:08 +0530

    responsive design

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,15 +4,19 @@ import "../App.css"
 function Footer() {
      
     return (
-        <Box display={{lg:'flex',md:"grid"}} flexWrap='wrap'>
+        <Box w={"full"}  h={10}  margin={'auto'}>
     <hr color="green" />
-        <Box display={{lg:'flex',md:"grid"}} flexWrap='wrap'>
+        <Box display={{base:'flex'}} width='70%' gap={7} textAlign='center' flexWrap='wrap' m={'auto'}>
             <h3>For Devlopers</h3>
             <Text>About</Text>
             <Text>High Scores</Text>
             <Text>FAQ</Text>
             <Text>Contact Us</Text>
-            <strong>Donate</strong>
+                <Text as={'button'} border={2} borderColor='yellow' color='teal.500'>
+                <strong>Donate</strong>
+                </Text>
+                
+                
             <Text>Privacy & Terms</Text>
             </Box>
 
