3b178fe1155a5cc7e68efe9539861f3873b3be7f
Brar21 <103635395+Brar21@users.noreply.github.com>
2023-02-07 12:17:08 +0530

    fix resposive error

--- src/Pages/Typingtest.jsx
+++ src/Pages/Typingtest.jsx
@@ -28,15 +28,15 @@ function Test() {
 
     return (
         <Box  w={{base:'auto',lg:'100%'}} h='auto' >
-            <Box bg='#577b87' h={{base:'117vh',md:"88vh",xl:'80vh'}} >
-                <Box w='70%' m='auto' display={{base:'grid',lg:'flex'}} flexDirection='row' gap={10}>
+            <Box bg='#577b87' h={{base: '108vh',md: "70vh",xl: '70vh'}} p={{base:'1rem',md:'0rem'}} >
+                <Box w={{base:'auto',sm:'70%'}}  m='auto' display={{base:'grid',md:'flex'}} flexDirection='row' gap={10}>
                     <Box>
                         <Image w='auto' borderRadius={20} src='https://i.pinimg.com/originals/3f/82/40/3f8240fa1d16d0de6d4e7510b43b37ba.gif' />
                     </Box>
                     <Box m='auto' _disabled='flex' flexDirection='column' >
                         <Heading textAlign='start' color='white'>Speed Test</Heading>
                         <Text textAlign='start' p='1.65rem 0' color='white' fontSize={18}>You can test your typing speed in wpm on English, Spanish, or French and impress your friends or employers with your own typing certificate. Test your typing speed, and find out how fast you type, and then improve your typing speed if necessary.</Text>
-                        <Button bg='#e7b75f' p='2rem' _hover={{ bg: "yellow.300", color: 'brown' }} fontSize='20px' onClick={onOpen}>
+                        <Button bg='#e7b75f' p={{base:'10px',md:'2rem'}} _hover={{ bg: "yellow.300", color: 'brown' }} fontSize='20px' onClick={onOpen}>
                             Take Free Typing Test
                         </Button>
 
@@ -76,7 +76,7 @@ function Test() {
                             <Table variant='simple'>
 
                                 <Thead w={{base:'auto',lg:'100%'}} >
-                                    <Tr display='flex' flexDirection='row' w={{base:'auto',lg:'100%'}} justifyContent='space-between'>
+                                    <Tr display={{base:'grid',md:'flex'}} flexDirection='row' m={'auto'} w={{base:'auto',md:'100%'}} justifyContent='space-between'>
                                         <Th display='flex' gap='5px' pl="1rem"><GrCertificate fontSize={{base:'auto',lg:20}} />CERTIFICATE</Th>
                                         <Th display='flex' gap='5px' ml='2rem'><SlSpeedometer fontSize={20} />SPEED</Th>
                                         <Th display='flex' gap='5px'><GiOnTarget fonrSize={20} />ACCURACY</Th>
@@ -84,19 +84,19 @@ function Test() {
                                 </Thead>
                                 <hr />
                                 <Tbody w={{base:'auto',lg:'100%'}} fontWeight='500' color={'black'}>
-                                    <Tr display='flex' flexDirection='row' w={{base:'auto',lg:'100%'}} justifyContent='space-between' alignItems='center'>
+                                    <Tr display={{base:'grid',md:'flex'}} flexDirection='row' w={{base:'auto',lg:'100%'}} justifyContent='space-between' alignItems='center'>
                                         <Td display='flex' textAlign='center'><Image w={30} src='https://cdn.vectorstock.com/i/preview-1x/65/82/silver-rosette-vector-996582.jpg' /><Text pt={2}>PLATINUM</Text></Td>
                                         <Td pl='5px'>70 WPM</Td>
                                         <Td>99.5%</Td>
                                     </Tr>
                                     <hr />
-                                    <Tr display='flex' flexDirection='row'w={{base:'auto',lg:'100%'}}justifyContent='space-between' color='black'>
+                                    <Tr display={{base:'grid',md:'flex'}} flexDirection='row'w={{base:'auto',lg:'100%'}}justifyContent='space-between' color='black'>
                                         <Td display='flex' textAlign='center' ><Image w={35} src='https://static.vecteezy.com/system/resources/previews/005/724/486/original/gold-circle-medal-with-red-ribbon-golden-round-trophy-for-first-place-of-competition-metal-award-for-leader-on-white-background-honor-victory-prize-isolated-illustration-vector.jpg' /><Text pt={3}>GOLD</Text></Td>
                                         <Td pr='5px'>50 WPM</Td>
                                         <Td>98.7%</Td>
                                     </Tr>
                                     <hr />
-                                    <Tr display='flex' flexDirection='row' w={{base:'auto',lg:'100%'}}justifyContent='space-between' color='black'>
+                                    <Tr display={{base:'grid',md:'flex'}} flexDirection='row' w={{base:'auto',lg:'100%'}}justifyContent='space-between' color='black'>
                                         <Td display='flex' textAlign='center'><Image w={30} src='https://t3.ftcdn.net/jpg/04/87/34/18/360_F_487341885_43bjb9IUYv8c7Fhxm5kp9KgvWgZUeNMo.jpg' /><Text pt={2}>SILVER</Text></Td>
                                         <Td>40 WPM</Td>
                                         <Td>96%</Td>
