0a3dcb99748fc9f1207b39bfa62c7858704960dc
Brar21 <103635395+Brar21@users.noreply.github.com>
2023-02-09 13:24:12 +0530

    google authentication for sign-in

--- Authentication/config.jsx
+++ Authentication/config.jsx
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {getAuth,GoogleAuthProvider} from 'firebase/auth'
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBJXx9-dbMw8OB411XGDW5_DrVKL2W-jt4",
   authDomain: "typemaster-8b132.firebaseapp.com",
@@ -17,5 +12,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const app=initializeApp(firebaseConfig);
+const auth=getAuth(app)
+const provider=new GoogleAuthProvider();
+export {auth,<provider> </provider>}
