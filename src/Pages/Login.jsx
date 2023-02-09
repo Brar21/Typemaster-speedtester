b14050ee97da1c4c0f5b7651b3e3a677f59eb832
Brar21 <103635395+Brar21@users.noreply.github.com>
2023-02-09 13:27:08 +0530

    google authentication for sign-in

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,54 +1,50 @@
-import { Button, HStack,Input,Heading,Text } from '@chakra-ui/react';
+import { Button, HStack, Input, Heading, Text } from "@chakra-ui/react";
 import { FaFacebook } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
-import { Container,Box,Image } from '@chakra-ui/react';
+import { Container, Box, Image } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
-import { LoginSocialFacebook } from "reactjs-social-login";
-import { useState } from 'react';
-
+import { auth,provider } from "../../Authentication/config";
+import { signInWithPopup } from "firebase/auth";
 function Login() {
-    const [Data, setData] = useState(null);
+    const handleClick=() =>
+    {
     
-    return <>
-        <Container  w='100%' h={'100vh'} mt='5rem'>
-            <Heading textAlign='start' margin='1rem 0'>Log in</Heading>
-            <HStack spacing='1.5rem' display={'flex'} gap='1rem' m={'auto'} wrap={'wrap'}>
-                {!Data ? <LoginSocialFacebook
-                    appId='523530702789841'
-                    onResolve={(res) => {
-                        console.log(res)
-                        setData(res.data)
-                    }}
-                    onReject={(err) => {
-                        console.log(err)
-                    }}
-                >
-                    
-                    <Button colorScheme='twitter' leftIcon={<FaFacebook />}>
-                        Log in with Facebook
-                    </Button>
-                </LoginSocialFacebook> : ''}
-                <Box>
-                {Data ? <><Heading>{Data.name}</Heading>
-                        <Image src={Data.picture.data.url} /></> : ''}
-                </Box>
-            
-            <Button colorScheme='facebook' leftIcon={<FcGoogle/>}>
-               Log in with Google
-            </Button>
-            </HStack>
-            <Text textAlign='start' margin='auto'>or you can login with your email:</Text>
-            <Box w='100%' >
+}
+  return (
+    <>
+      <Container w="100%" h={"100vh"} mt="5rem">
+        <Heading textAlign="start" margin="1rem 0">
+          Log in
+        </Heading>
+        <HStack
+          spacing="1.5rem"
+          display={"flex"}
+          gap="1rem"
+          m={"auto"}
+          wrap={"wrap"}
+        >
+          <Button colorScheme="twitter" leftIcon={<FaFacebook />} onClick={handleClick}>
+            Log in with Facebook
+          </Button>
 
-            <Input placeholder='Email Address' type='email' margin='1rem 0'/>
-                <Input placeholder='Enter Password' type='password' margin='1rem 0' />
-                <Box  marginBottom='1rem' marginTop='1rem'>
-                    <Button colorScheme='blue'>LOG IN</Button> <strong>or</strong> <Link to='/signup'>Sign Up now</Link>
-                </Box>
-                <Link to='/resetpassword'>Forget your password?</Link>
-            </Box>
-            
-        </Container>
+          <Button colorScheme="facebook" leftIcon={<FcGoogle />}>
+            Log in with Google
+          </Button>
+        </HStack>
+        <Text textAlign="start" margin="auto">
+          or you can login with your email:
+        </Text>
+        <Box w="100%">
+          <Input placeholder="Email Address" type="email" margin="1rem 0" />
+          <Input placeholder="Enter Password" type="password" margin="1rem 0" />
+          <Box marginBottom="1rem" marginTop="1rem">
+            <Button colorScheme="blue">LOG IN</Button> <strong>or</strong>{" "}
+            <Link to="/signup">Sign Up now</Link>
+          </Box>
+          <Link to="/resetpassword">Forget your password?</Link>
+        </Box>
+      </Container>
     </>
+  );
 }
 export default Login;
