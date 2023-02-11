65ab5dc951cbc601d6af4a3b3d96f69a1132e73a
Brar21 <103635395+Brar21@users.noreply.github.com>
2023-02-11 15:24:22 +0530

    Adding Profile Page

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,125 +1,133 @@
 import {
-    Button,
-    Flex,
-    FormControl,
-    FormLabel,
-    Heading,
-    Input,
-    Stack,
-    useColorModeValue,
-    Avatar,
-    AvatarBadge,
-    IconButton,
-    Center,
-  } from '@chakra-ui/react';
-  import { CheckIcon } from '@chakra-ui/icons';
-import {useState} from 'react';
-  
-export default function Profile()
-{
-  const data=JSON.parse(localStorage.getItem('emaildetail'))
-    const [name,setName]=useState(data.user.displayName)
-    const [email,setEmail]=useState(data.user.email)
-    const [password,setPassword]=useState("")
-    
-    const handleChange=(e) =>
-    {
-        setEmail(email)
-        setName(name)
-        setPassword(e.value)
-    }
-    return (
-      <Flex
-        minH={'100vh'}
-        align={'center'}
-        justify={'center'}
-        bg={useColorModeValue('gray.50', 'gray.800')}>
-        <Stack
-          spacing={4}
-          w={'full'}
-          maxW={'md'}
-          bg={useColorModeValue('white', 'gray.700')}
-          rounded={'xl'}
-          boxShadow={'lg'}
-          p={6}
-          my={12}>
-          <Heading lineHeight={1.1} fontSize={{ base: '2xl', sm: '3xl' }}>
-            User Profile Edit
-          </Heading>
-          <FormControl id="userName">
-            <FormLabel>User Icon</FormLabel>
-            <Stack direction={['column', 'row']} spacing={6}>
-              <Center>
-                <Avatar size="xl" src={data.user.photoURL}>
-                  <AvatarBadge
-                    as={IconButton}
-                    size="xs"
-                    rounded="full"
-                    top="-10px"
-                    colorScheme="green"
-                    aria-label="remove Image"
-                    icon={<CheckIcon />}
-                  />
-                </Avatar>
-              </Center>
-              <Center w="full">
-                <Button w="full">Change Icon</Button>
-              </Center>
-            </Stack>
-          </FormControl>
-          <FormControl id="userName" isRequired>
-            <FormLabel>User name</FormLabel>
-            <Input
-              placeholder="UserName"
-              _placeholder={{ color: 'gray.500' }}
-                        type="text"
-                        value={name}
-                        onChange={handleChange}
-                        disabled
-            />
-          </FormControl>
-          <FormControl id="email" isRequired>
-            <FormLabel>Email address</FormLabel>
-            <Input
-              placeholder="your-email@example.com"
-              _placeholder={{ color: 'gray.500' }}
-                        type="email"
-                        value={email}
-                        onChange={handleChange}
-                        disabled
-            />
-          </FormControl>
-          <FormControl id="password" isRequired>
-            <FormLabel>Password</FormLabel>
-            <Input
-              placeholder="password"
-              _placeholder={{ color: 'gray.500' }}
-                        type="password"
-                        value={password}
-                        onChange={handleChange}
-            />
-          </FormControl>
-          <Stack spacing={6} direction={['column', 'row']}>
-            <Button
-              bg={'red.400'}
-              color={'white'}
-              w="full"
-              _hover={{
-                bg: 'red.500',
-              }}>
-              Cancel
-            </Button>
-            <Button
-              bg={'blue.400'}
-              color={'white'}
-              w="full"
-              _hover={{
-                bg: 'blue.500',
-              }}>
-              Submit
-            </Button>
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Stack,
+  useColorModeValue,
+  Avatar,
+  AvatarBadge,
+  IconButton,
+  Center,
+} from "@chakra-ui/react";
+import { CheckIcon } from "@chakra-ui/icons";
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
+
+export default function Profile() {
+  const data = JSON.parse(localStorage.getItem("emaildetail"));
+  const [name, setName] = useState(data.user.displayName);
+  const [email, setEmail] = useState(data.user.email);
+  const [password, setPassword] = useState("");
+ const [Cancel,setCancel]=useState(false)
+  const handleCancel = () => {
+    setCancel(true)
+  };
+
+  const handleChange = (e) => {
+    setEmail(email);
+    setName(name);
+    setPassword(e.value);
+  };
+  return (
+    <Flex
+      minH={"100vh"}
+      align={"center"}
+      justify={"center"}
+      bg={useColorModeValue("gray.50", "gray.800")}
+    >
+      <Stack
+        spacing={4}
+        w={"full"}
+        maxW={"md"}
+        bg={useColorModeValue("white", "gray.700")}
+        rounded={"xl"}
+        boxShadow={"lg"}
+        p={6}
+        my={12}
+      >
+        <Heading lineHeight={1.1} fontSize={{ base: "2xl", sm: "3xl" }}>
+          User Profile Edit
+        </Heading>
+        <FormControl id="userName">
+          <FormLabel>User Icon</FormLabel>
+          <Stack direction={["column", "row"]} spacing={6}>
+            <Center>
+              <Avatar size="xl" src={data.user.photoURL}>
+                <AvatarBadge
+                  as={IconButton}
+                  size="xs"
+                  rounded="full"
+                  top="-10px"
+                  colorScheme="green"
+                  aria-label="remove Image"
+                  icon={<CheckIcon />}
+                />
+              </Avatar>
+            </Center>
+            <Center w="full">
+              <Button w="full">Change Icon</Button>
+            </Center>
           </Stack>
+        </FormControl>
+        <FormControl id="userName" isRequired>
+          <FormLabel>User name</FormLabel>
+          <Input
+            placeholder="UserName"
+            _placeholder={{ color: "gray.500" }}
+            type="text"
+            value={name}
+            onChange={handleChange}
+            disabled
+          />
+        </FormControl>
+        <FormControl id="email" isRequired>
+          <FormLabel>Email address</FormLabel>
+          <Input
+            placeholder="your-email@example.com"
+            _placeholder={{ color: "gray.500" }}
+            type="email"
+            value={email}
+            onChange={handleChange}
+            disabled
+          />
+        </FormControl>
+        <FormControl id="password" isRequired>
+          <FormLabel>Password</FormLabel>
+          <Input
+            placeholder="password"
+            _placeholder={{ color: "gray.500" }}
+            type="password"
+            value={password}
+            onChange={handleChange}
+          />
+        </FormControl>
+        <Stack spacing={6} direction={["column", "row"]}>
+          {Cancel?<Navigate to='/' />:<Button
+            bg={"red.400"}
+            color={"white"}
+            w="full"
+            _hover={{
+              bg: "red.500",
+            }}
+            onClick={handleCancel}
+          >
+            Cancel
+          </Button>}
+          <Button
+            bg={"blue.400"}
+            color={"white"}
+            w="full"
+            _hover={{
+              bg: "blue.500",
+            }}
+          >
+            Submit
+          </Button>
         </Stack>
-      </Flex>
-    );
-  }
+      </Stack>
+    </Flex>
+  );
+}
