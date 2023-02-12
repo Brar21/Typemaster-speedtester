1f30546cfb08d763f752b74f0814b7e3214ae374
Brar21 <103635395+Brar21@users.noreply.github.com>
2023-02-12 10:28:19 +0530

    signup form

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,9 +1,10 @@
 import { Button, Input, Heading, Text } from '@chakra-ui/react';
 import { Container, Box,Stack,Checkbox } from '@chakra-ui/react';
+import {useState} from 'react';
 import { Link } from "react-router-dom";
 
 function Signup() {
-
+const [state,setState]=useState({name:"",email:"",password:"",confirmpassword:""})
     return <>
             <Container w='100%' h={'100vh'}>
             <Heading textAlign='start' margin='1rem 0'>Create a free Account</Heading>
@@ -11,11 +12,11 @@ function Signup() {
                
                 <Text textAlign='start' margin='1rem 0'>or you can Sign Up with your email:</Text>
                 <Box w='100%' >
-                <Input placeholder='Your Name' type='text' margin='1rem 0' />
+                <Input placeholder='Your Name' name='' value={} onChange={handleChange} type='text' margin='1rem 0' />
 
-                    <Input placeholder='Email Address' type='email' margin='1rem 0' />
-                <Input placeholder='Enter Password' type='password' margin='1rem 0' />
-                <Input placeholder='Confirm Password' type='password' margin='1rem 0' />
+                    <Input placeholder='Email Address' name='' value={} onChange={handleChange} type='email' margin='1rem 0' />
+                <Input placeholder='Enter Password' name='' value={} onChange={handleChange} type='password' margin='1rem 0' />
+                <Input placeholder='Confirm Password' name='' value={} onChange={handleChange} type='password' margin='1rem 0' />
 
                 <Stack spacing={5} direction='column'>
                     <Checkbox w='18rem' colorScheme='blue' defaultChecked>
