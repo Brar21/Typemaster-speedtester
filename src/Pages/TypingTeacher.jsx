de03f2bed9529255b8790286e0e4dfe773523d3a
Brar21 <103635395+Brar21@users.noreply.github.com>
2023-02-27 20:57:20 +0530

    resposive response

--- src/Pages/TypingTeacher.jsx
+++ src/Pages/TypingTeacher.jsx
@@ -4,7 +4,7 @@ function TypingTeacher() {
 
     return (
         <Box w={'90%'} m={'auto'}>
-            <Heading mt={10} mb={{lg:-20}}>Coming Soon</Heading>
+            <Heading mt={10} mb={{lg:-20}}>Coming Soon...</Heading>
             <Image src={png} alt={png} w='100%' />
         </Box>
     )
