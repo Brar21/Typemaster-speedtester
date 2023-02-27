d36483970ecc59476604a3f1add09afa95020e27
Brar21 <103635395+Brar21@users.noreply.github.com>
2023-02-27 17:39:57 +0530

    logo updation

--- src/Pages/TypingTeacher.jsx
+++ src/Pages/TypingTeacher.jsx
@@ -1,11 +1,12 @@
-
+import {Box,Heading,Image} from "@chakra-ui/react";
+import png from "./web-maintenance.svg"
 function TypingTeacher() {
 
     return (
-        <div>
-            <h1>Learning Typing</h1>
-
-        </div>
+        <Box w={'90%'} m={'auto'}>
+            <Heading mt={10} mb={{lg:-20}}>Coming Soon</Heading>
+            <Image src={png} alt={png} w='100%' />
+        </Box>
     )
 }
 export default TypingTeacher;
